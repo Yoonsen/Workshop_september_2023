8cee262684367178ffa5ae4c7ccbc1027ac47682
Lars G Johnsen <yoonsen@gmail.com>
2023-12-24 13:27:30 +0100

    Update bbi_app.py
    debug

--- bbi_app.py
+++ bbi_app.py
@@ -27,7 +27,7 @@ def korpus():
     return kudos.dropna(), barn.dropna()
 
 kudos, barn = korpus()
-
+st.write(len(kudos), len(barn))
 splits = [1945, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, 2030]
 groups = [splits[a:a+2] for a in range(len(splits)-1)]
 make_splits = lambda df: {f"{str(x[0])}-{str(x[1])}": df.loc[barn.year >= x[0]].loc[df.year < x[1]] for x in groups}
